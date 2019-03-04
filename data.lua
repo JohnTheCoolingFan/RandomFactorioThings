aa942a69e586a0f17cd249e50fb91fdc228b5dd0
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-03-04 07:33:12 +0300

    Switched to LX (UNIX) line endings.

--- data.lua
+++ data.lua
@@ -1,6 +1,6 @@
-require("prototypes.categories.recipe-category")
+require("prototypes.categories.recipe-category")  -- Adding categories by separate file, because categories don't have one main file
 require("prototypes.categories.module-category")
-require("prototypes.entity.entities")
+require("prototypes.entity.entities")             -- Other prototype types have main files in which specific files will be require()d.
 require("prototypes.item.item")
 require("prototypes.recipe.recipe")
 require("prototypes.technology.technology")
