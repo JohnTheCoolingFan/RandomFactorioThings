ed4764f159ae2c5f67df51e43ce283310b29b71c
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-04-28 16:35:25 +0300

    Added plutonium steel item

--- prototypes/item/item.lua
+++ prototypes/item/item.lua
@@ -110,6 +110,12 @@ raw_nuclear_metal.name = "raw-nuclear-metal"
 raw_nuclear_metal.icon = "__RandomFactorioThings__/graphics/icons/raw-nuclear-metal.png"
 table.insert(items, raw_nuclear_metal)
 
+if mods["PlutoniumEnergy"] then
+local plutonium_steel = util.table.deepcopy(data.raw["item"]["steel-plate"])
+plutonium_steel.name = "plutonium-steel"
+plutonium_steel.icon = "__RandomFactorioThings__/graphics/icons/plutonium-steel.png"
+end
+
 local nuclear_long_handed_inserter = util.table.deepcopy(data.raw["item"]["long-handed-inserter"])
 nuclear_long_handed_inserter.name = "nuclear-long-handed-inserter"
 nuclear_long_handed_inserter.icon = "__RandomFactorioThings__/graphics/icons/nuclear-long-handed-inserter.png"
