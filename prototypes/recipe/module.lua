5064a9d423b67ffa5e6a875c9e65f6f6ee54f1a6
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-04-30 23:02:04 +0300

    Indentation formatting

--- prototypes/recipe/module.lua
+++ prototypes/recipe/module.lua
@@ -1,244 +1,244 @@
 -- TODO: module recipes
 data:extend({
     {
-		type = "recipe",
-		name = "speed-module-4",
-		enabled = false,
-		ingredients =
-		{
-			{"speed-module-3", 5},
-			{"advanced-circuit", 10},
-			{"processing-unit", 10}
-		},
-		energy_required = 120,
-		result = "speed-module-4"
-	},
-	{
-		type = "recipe",
-		name = "productivity-module-4",
-		enabled = false,
-		ingredients =
-		{
-			{"productivity-module-3", 5},
-			{"advanced-circuit", 10},
-			{"processing-unit", 10}
-		},
-		energy_required = 120,
-		result = "productivity-module-4"
-	},
-	{
-		type = "recipe",
-		name = "effectivity-module-4",
-		enabled = false,
-		ingredients =
-		{
-			{"effectivity-module-3", 5},
-			{"advanced-circuit", 10},
-			{"processing-unit", 10}
-		},
-		energy_required = 120,
-		result = "effectivity-module-4"
-	},
-	{
-		type = "recipe",
-		name = "hardened-electric-mining-drill",
-		normal =
-		{
-			enabled = false,
-			energy_required = 2,
-			ingredients =
-			{
-				{"electronic-circuit", 5},
-				{"iron-gear-wheel", 10},
-				{"iron-plate", 20}
-			},
-			result = "hardened-electric-mining-drill"
-		},
-		expensive =
-		{
-			enabled = false,
-			energy_required = 2,
-			ingredients =
-			{
-				{"electronic-circuit", 10},
-				{"iron-gear-wheel", 20},
-				{"iron-plate", 40}
-			},
-			result = "hardened-electric-mining-drill"
-		}
-	},
-	{
-		type = "recipe",
-		name = "speed-effectivity-module",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 2},
-			{"electronic-circuit", 2},
-			{"speed-module", 1},
-			{"effectivity-module", 1}
-		},
-		energy_required = 10,
-		result = "speed-effectivity-module"
-	},
-	{
-		type = "recipe",
-		name = "speed-effectivity-module-2",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 3},
-			{"electronic-circuit", 3},
-			{"speed-module-2", 1},
-			{"effectivity-module-2", 1}
-		},
-		energy_required = 20,
-		result = "speed-effectivity-module-2"
-	},
-	{
-		type = "recipe",
-		name = "speed-effectivity-module-3",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 4},
-			{"electronic-circuit", 4},
-			{"speed-module-2", 1},
-			{"effectivity-module-2", 1},
-			{"processing-unit", 1}
-		},
-		energy_required = 30,
-		result = "speed-effectivity-module-3"
-	},
-	{
-		type = "recipe",
-		name = "speed-effectivity-module-4",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 5},
-			{"electronic-circuit", 5},
-			{"speed-module-3", 1},
-			{"effectivity-module-3", 1},
-			{"processing-unit", 2}
-		},
-		energy_required = 40,
-		result = "speed-effectivity-module-4"
-	},
-	{
-		type = "recipe",
-		name = "productivity-effectivity-module",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 2},
-			{"electronic-circuit", 2},
-			{"productivity-module", 1},
-			{"effectivity-module", 1}
-		},
-		energy_required = 10,
-		result = "productivity-effectivity-module"
-	},
-	{
-		type = "recipe",
-		name = "productivity-effectivity-module-2",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 3},
-			{"electronic-circuit", 3},
-			{"productivity-module-2", 1},
-			{"effectivity-module-2", 1}
-		},
-		energy_required = 20,
-		result = "productivity-effectivity-module-2"
-	},
-	{
-		type = "recipe",
-		name = "productivity-effectivity-module-3",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 4},
-			{"electronic-circuit", 4},
-			{"productivity-module-3", 1},
-			{"effectivity-module-3", 1},
-			{"processing-unit", 1}
-		},
-		energy_required = 30,
-		result = "productivity-effectivity-module-3"
-	},
-	{
-		type = "recipe",
-		name = "productivity-effectivity-module-4",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 5},
-			{"electronic-circuit", 5},
-			{"productivity-module-4", 1},
-			{"effectivity-module-4", 1},
-			{"processing-unit", 2}
-		},
-		energy_required = 40,
-		result = "productivity-effectivity-module-4"
-	},
-	{
-		type = "recipe",
-		name = "speed-productivity-module",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 2},
-			{"electronic-circuit", 2},
-			{"speed-module", 1},
-			{"productivity-module", 1}
-		},
-		energy_required = 10,
-		result = "speed-productivity-module"
-	},
-	{
-		type = "recipe",
-		name = "speed-productivity-module-2",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 3},
-			{"electronic-circuit", 3},
-			{"speed-module-2", 1},
-			{"productivity-module-2", 1}
-		},
-		energy_required = 20,
-		result = "speed-productivity-module-2"
-	},
-	{
-		type = "recipe",
-		name = "speed-productivity-module-3",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 4},
-			{"electronic-circuit", 4},
-			{"speed-module-2", 1},
-			{"productivity-module-2", 1},
-			{"processing-unit", 1}
-		},
-		energy_required = 30,
-		result = "speed-productivity-module-3"
-	},
-	{
-		type = "recipe",
-		name = "speed-productivity-module-4",
-		enabled = false,
-		ingredients =
-		{
-			{"advanced-circuit", 5},
-			{"electronic-circuit", 5},
-			{"speed-module-3", 1},
-			{"productivity-module-3", 1},
-			{"processing-unit", 2}
-		},
-		energy_required = 40,
-		result = "speed-productivity-module-4"
-	}
+        type = "recipe",
+        name = "speed-module-4",
+        enabled = false,
+        ingredients =
+        {
+            {"speed-module-3", 5},
+            {"advanced-circuit", 10},
+            {"processing-unit", 10}
+        },
+        energy_required = 120,
+        result = "speed-module-4"
+    },
+    {
+        type = "recipe",
+        name = "productivity-module-4",
+        enabled = false,
+        ingredients =
+        {
+            {"productivity-module-3", 5},
+            {"advanced-circuit", 10},
+            {"processing-unit", 10}
+        },
+        energy_required = 120,
+        result = "productivity-module-4"
+    },
+    {
+        type = "recipe",
+        name = "effectivity-module-4",
+        enabled = false,
+        ingredients =
+        {
+            {"effectivity-module-3", 5},
+            {"advanced-circuit", 10},
+            {"processing-unit", 10}
+        },
+        energy_required = 120,
+        result = "effectivity-module-4"
+    },
+    {
+        type = "recipe",
+        name = "hardened-electric-mining-drill",
+        normal =
+        {
+            enabled = false,
+            energy_required = 2,
+            ingredients =
+            {
+                {"electronic-circuit", 5},
+                {"iron-gear-wheel", 10},
+                {"iron-plate", 20}
+            },
+            result = "hardened-electric-mining-drill"
+        },
+        expensive =
+        {
+            enabled = false,
+            energy_required = 2,
+            ingredients =
+            {
+                {"electronic-circuit", 10},
+                {"iron-gear-wheel", 20},
+                {"iron-plate", 40}
+            },
+            result = "hardened-electric-mining-drill"
+        }
+    },
+    {
+        type = "recipe",
+        name = "speed-effectivity-module",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 2},
+            {"electronic-circuit", 2},
+            {"speed-module", 1},
+            {"effectivity-module", 1}
+        },
+        energy_required = 10,
+        result = "speed-effectivity-module"
+    },
+    {
+        type = "recipe",
+        name = "speed-effectivity-module-2",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 3},
+            {"electronic-circuit", 3},
+            {"speed-module-2", 1},
+            {"effectivity-module-2", 1}
+        },
+        energy_required = 20,
+        result = "speed-effectivity-module-2"
+    },
+    {
+        type = "recipe",
+        name = "speed-effectivity-module-3",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 4},
+            {"electronic-circuit", 4},
+            {"speed-module-2", 1},
+            {"effectivity-module-2", 1},
+            {"processing-unit", 1}
+        },
+        energy_required = 30,
+        result = "speed-effectivity-module-3"
+    },
+    {
+        type = "recipe",
+        name = "speed-effectivity-module-4",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 5},
+            {"electronic-circuit", 5},
+            {"speed-module-3", 1},
+            {"effectivity-module-3", 1},
+            {"processing-unit", 2}
+        },
+        energy_required = 40,
+        result = "speed-effectivity-module-4"
+    },
+    {
+        type = "recipe",
+        name = "productivity-effectivity-module",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 2},
+            {"electronic-circuit", 2},
+            {"productivity-module", 1},
+            {"effectivity-module", 1}
+        },
+        energy_required = 10,
+        result = "productivity-effectivity-module"
+    },
+    {
+        type = "recipe",
+        name = "productivity-effectivity-module-2",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 3},
+            {"electronic-circuit", 3},
+            {"productivity-module-2", 1},
+            {"effectivity-module-2", 1}
+        },
+        energy_required = 20,
+        result = "productivity-effectivity-module-2"
+    },
+    {
+        type = "recipe",
+        name = "productivity-effectivity-module-3",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 4},
+            {"electronic-circuit", 4},
+            {"productivity-module-3", 1},
+            {"effectivity-module-3", 1},
+            {"processing-unit", 1}
+        },
+        energy_required = 30,
+        result = "productivity-effectivity-module-3"
+    },
+    {
+        type = "recipe",
+        name = "productivity-effectivity-module-4",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 5},
+            {"electronic-circuit", 5},
+            {"productivity-module-4", 1},
+            {"effectivity-module-4", 1},
+            {"processing-unit", 2}
+        },
+        energy_required = 40,
+        result = "productivity-effectivity-module-4"
+    },
+    {
+        type = "recipe",
+        name = "speed-productivity-module",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 2},
+            {"electronic-circuit", 2},
+            {"speed-module", 1},
+            {"productivity-module", 1}
+        },
+        energy_required = 10,
+        result = "speed-productivity-module"
+    },
+    {
+        type = "recipe",
+        name = "speed-productivity-module-2",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 3},
+            {"electronic-circuit", 3},
+            {"speed-module-2", 1},
+            {"productivity-module-2", 1}
+        },
+        energy_required = 20,
+        result = "speed-productivity-module-2"
+    },
+    {
+        type = "recipe",
+        name = "speed-productivity-module-3",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 4},
+            {"electronic-circuit", 4},
+            {"speed-module-2", 1},
+            {"productivity-module-2", 1},
+            {"processing-unit", 1}
+        },
+        energy_required = 30,
+        result = "speed-productivity-module-3"
+    },
+    {
+        type = "recipe",
+        name = "speed-productivity-module-4",
+        enabled = false,
+        ingredients =
+        {
+            {"advanced-circuit", 5},
+            {"electronic-circuit", 5},
+            {"speed-module-3", 1},
+            {"productivity-module-3", 1},
+            {"processing-unit", 2}
+        },
+        energy_required = 40,
+        result = "speed-productivity-module-4"
+    }
 })
