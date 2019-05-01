fb4dfa3bc634b2c60a7e06de2d3bf8b2fd06b10f
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-05-01 13:52:48 +0300

    Moved all module technologies to separate file

--- prototypes/technology/module-technology.lua
+++ prototypes/technology/module-technology.lua
@@ -3,6 +3,112 @@
 - Rebalance existing technologies
 ]]
 data:extend({
+    -- Speed
+    {
+		type = "technology",
+		name = "speed-module-4",
+		icon = "__base__/graphics/technology/speed-module.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "speed-module-4"
+			}
+		},
+		prerequisites = {"speed-module-3"},
+		unit =
+		{
+			count = 400,
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"utility-science-pack", 1}
+			},
+			time = 60
+		},
+		upgrade = true,
+		order = "i-c-c" -- TODO
+	},
+    -- Productivity
+	{
+		type = "technology",
+		name = "productivity-module-4",
+		icon = "__base__/graphics/technology/productivity-module.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "productivity-module-4"
+			}
+		},
+		prerequisites = {"productivity-module-3"},
+		unit =
+		{
+			count = 400,
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"production-science-pack", 1}
+			},
+			time = 60
+		},
+		upgrade = true,
+		order = "i-c-c" -- TODO
+	},
+    -- Effectivity
+	{
+		type = "technology",
+		name = "effectivity-module-4",
+		icon = "__base__/graphics/technology/effectivity-module.png",
+		icon_size = 128,
+		effects =
+		{
+			{
+				type = "unlock-recipe",
+				recipe = "effectivity-module-4"
+			}
+		},
+		prerequisites = {"effectivity-module-3"},
+		unit =
+		{
+			count = 400,
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1},
+				{"chemical-science-pack", 1},
+				{"production-science-pack", 1}
+			},
+			time = 60
+		},
+		upgrade = true,
+		order = "i-c-c" -- TODO
+	},
+    -- Merged
+    {
+		type = "technology",
+		name = "merged-modules",
+		icon = "__RandomFactorioThings__/graphics/technology/merged-modules.png",
+		icon_size = 128,
+		prerequisites = {"modules"},
+		unit =
+		{
+			count = 200,
+			ingredients =
+			{
+				{"automation-science-pack", 1},
+				{"logistic-science-pack", 1}
+			},
+			time = 30
+		},
+		order = "i-a" -- TODO
+	},
     -- Speed-effectivity
     {
         type = "technology",
