7d19fee9715b56d5b89747cffaee07910aa6c9d4
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-05-01 14:02:28 +0300

    Some code formatting

--- prototypes/technology/module-technology.lua
+++ prototypes/technology/module-technology.lua
@@ -238,11 +238,9 @@ data:extend({
         icon = "__RandomFactorioThings__/graphics/technology/merged-modules.png",
         icon_size = 128,
         prerequisites = {"modules"},
-        unit =
-        {
+        unit = {
             count = 200,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1}
             },
@@ -256,19 +254,16 @@ data:extend({
         name = "speed-effectivity-module",
         icon = "__RandomFactorioThings__/graphics/technology/speed-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "speed-effectivity-module"
             }
         },
         prerequisites = {"merged-modules", "speed-module", "effectivity-module"},
-        unit =
-        {
+        unit = {
             count = 50,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
             },
@@ -282,19 +277,16 @@ data:extend({
         name = "speed-effectivity-module-2",
         icon = "__RandomFactorioThings__/graphics/technology/speed-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects =     {
             {
                 type = "unlock-recipe",
                 recipe = "speed-effectivity-module-2"
             }
         },
         prerequisites = {"speed-effectivity-module", "speed-module-2", "effectivity-module-2"},
-        unit =
-        {
+        unit = {
             count = 50,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -309,19 +301,16 @@ data:extend({
         name = "speed-effectivity-module-3",
         icon = "__RandomFactorioThings__/graphics/technology/speed-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "speed-effectivity-module-3"
             }
         },
         prerequisites = {"speed-effectivity-module-2", "speed-module-3", "effectivity-module-3"},
-        unit =
-        {
+        unit = {
             count = 100,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -336,19 +325,16 @@ data:extend({
         name = "speed-effectivity-module-4",
         icon = "__RandomFactorioThings__/graphics/technology/speed-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "speed-effectivity-module-4"
             }
         },
         prerequisites = {"speed-effectivity-module-3", "speed-module-4", "effectivity-module-4"},
-        unit =
-        {
+        unit = {
             count = 100,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -364,19 +350,16 @@ data:extend({
         name = "productivity-effectivity-module",
         icon = "__RandomFactorioThings__/graphics/technology/productivity-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "productivity-effectivity-module"
             }
         },
         prerequisites = {"merged-modules", "productivity-module", "effectivity-module"},
-        unit =
-        {
+        unit = {
             count = 50,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1}
             },
@@ -390,19 +373,16 @@ data:extend({
         name = "productivity-effectivity-module-2",
         icon = "__RandomFactorioThings__/graphics/technology/productivity-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "productivity-effectivity-module-2"
             }
         },
         prerequisites = {"productivity-effectivity-module", "productivity-module-2", "effectivity-module-2"},
-        unit =
-        {
+        unit = {
             count = 50,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -417,19 +397,16 @@ data:extend({
         name = "productivity-effectivity-module-3",
         icon = "__RandomFactorioThings__/graphics/technology/productivity-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "productivity-effectivity-module-3"
             }
         },
         prerequisites = {"productivity-effectivity-module-2", "productivity-module-3", "effectivity-module-3"},
-        unit =
-        {
+        unit = {
             count = 100,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -444,19 +421,16 @@ data:extend({
         name = "productivity-effectivity-module-4",
         icon = "__RandomFactorioThings__/graphics/technology/productivity-effectivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "productivity-effectivity-module-4"
             }
         },
         prerequisites = {"productivity-effectivity-module-3", "productivity-module-4", "effectivity-module-4"},
-        unit =
-        {
+        unit = {
             count = 100,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -472,19 +446,16 @@ data:extend({
         name = "speed-productivity-module",
         icon = "__RandomFactorioThings__/graphics/technology/speed-productivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "speed-productivity-module"
             }
         },
         prerequisites = {"merged-modules", "speed-module", "productivity-module"},
-        unit =
-        {
+        unit = {
             count = 50,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
             },
@@ -498,19 +469,16 @@ data:extend({
         name = "speed-productivity-module-2",
         icon = "__RandomFactorioThings__/graphics/technology/speed-productivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "speed-productivity-module-2"
             }
         },
         prerequisites = {"speed-productivity-module", "speed-module-2", "productivity-module-2"},
-        unit =
-        {
+        unit = {
             count = 50,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -525,19 +493,16 @@ data:extend({
         name = "speed-productivity-module-3",
         icon = "__RandomFactorioThings__/graphics/technology/speed-productivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "speed-productivity-module-3"
             }
         },
         prerequisites = {"speed-productivity-module-2", "speed-module-3", "productivity-module-3"},
-        unit =
-        {
+        unit = {
             count = 100,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
@@ -552,19 +517,16 @@ data:extend({
         name = "speed-productivity-module-4",
         icon = "__RandomFactorioThings__/graphics/technology/speed-productivity-module.png",
         icon_size = 128,
-        effects =
-        {
+        effects = {
             {
                 type = "unlock-recipe",
                 recipe = "speed-productivity-module-4"
             }
         },
         prerequisites = {"speed-productivity-module-3", "speed-module-4", "productivity-module-4"},
-        unit =
-        {
+        unit = {
             count = 100,
-            ingredients =
-            {
+            ingredients = {
                 {"automation-science-pack", 1},
                 {"logistic-science-pack", 1},
                 {"military-science-pack", 1}
