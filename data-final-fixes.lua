3f6402af48e39b98933b602437c2be14705a4aef
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-06-01 14:08:10 +0300

    New deadlock mods integration

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,46 +4,59 @@
 - Change items that can be compressed/crated
 ]]
 
+-- Deadlock (deadlock-beltboxes-loaders) integration
 
---[[ Deprecated integration
+if deadlock then
 
---deadlock loaders integration
-if deadlock_loaders then
-    deadlock_loaders.create({
-        tier = 4,
+    -- Tier table
+    local nuclear_tier_table = {
         transport_belt = "nuclear-transport-belt",
+        colour = {g = 1},
         underground_belt = "nuclear-underground-belt",
         splitter = "nuclear-splitter",
-        colour = {r = 0, g = 255, b = 0},
-        ingredients = {
-            {"nuclear-metal", 2},
-            {"iron-gear-wheel", 10},
-            {"nuclear-transport-belt", 1},
-            {type="fluid", name="lubricant", amount=10}
-        },
-        crafting_category = "crafting-with-fluid",
         technology = "nuclear-logistics",
-        localisation_prefix = "nuclear"
-    })
-end
+        --order = "", -- TODO
+        loader_category = "crafting-with-fluid",
+        beltbox_technology = "nuclear-logistics",
+        beltbox_category = "crafting-with-fluid"
+    }
 
---DSB integration
-if deadlock_crafting then
-    deadlock_stacking.create("coal-piece", nil)
-    deadlock_stacking.create("hardened-furnace", nil, "JohnTheCF-processing")
-    deadlock_stacking.create("sawdust", nil)
-    deadlock_stacking.create("compressed-fuel", nil, "compressor")
-    deadlock_stacking.create("nuclear-metal", nil, "nuclear-automation")
-    deadlock_stacking.create("raw-nuclear-metal", nil, "nuclear-automation")
-end
+    -- Adding tier
+    deadlock.add_tier(nuclear_tier_table)
 
---DCM integration
-if deadlock_crating then
-    deadlock_crating.create("coal-piece", "deadlock-crating-1")
-    deadlock_crating.create("hardened-furnace", "JohnTheCF-processing")
-    deadlock_crating.create("sawdust", "deadlock-crating-1")
-    deadlock_crating.create("compressed-fuel", "compressor")
-    deadlock_crating.create("nuclear-metal", "nuclear-automation")
-    deadlock_crating.create("raw-nuclear-metal", "nuclear-automation")
+    -- Stacking
+    deadlock.add_stack("sawdust", "__RandomFactorioThings__/graphics/icons/sawdust.png", "deadlock-stacking-1", 32)
+    deadlock.add_stack("compressed-fuel", "__RandomFactorioThings__/graphics/icons/compressed-fuel.png", "deadlock-stacking-2", 32)
+    deadlock.add_stack("nuclear-metal", "__RandomFactorioThings__/graphics/icons/nuclear-metal.png", "deadlock-stacking-3", 32)
+    deadlock.add_stack("raw-nuclear-metal", "__RandomFactorioThings__/graphics/icons/raw-nuclear-metal.png", "deadlock-stacking-3", 32)
+
+    -- Plutonium
+    if mods["PlutoniumEnergy"] then
+
+        -- Tier table
+        local plutonium_tier_table = {
+            transport_belt = "plutonium-transport-belt",
+            colour = {r=0,g=1,b=0},
+            underground_belt = "plutonium-underground-belt",
+            splitter = "plutonium-splitter",
+            technology = "plutonium-logistics",
+            --order = "", -- TODO
+            loader_category = "crafting-with-fluid",
+            beltbox_technology = "plutonium-logistics",
+            beltbox_category = "crafting-with-fluid"
+        }
+
+        -- Adding tier
+        deadlock.add_tier(plutonium_tier_table)
+
+        -- Stacking
+        deadlock.add_stack("plutonium-steel", "__RandomFactorioThings__/graphics/icons/plutonium-steel.png", "deadlock-stacking-3", 32)
+        deadlock.add_stack("raw-plutonium-steel", "__RandomFactorioThings__/graphics/icons/raw-plutonium-steel.png", "deadlock-stacking-3", 32)
+        deadlock.add_stack("plutonium-239", "__PlutoniumEnergy__/graphics/icons/plutonium-239.png", 32)
+        deadlock.add_stack("plutonium-238", "__PlutoniumEnergy__/graphics/icons/plutonium-238.png", 32)
+        deadlock.add_stack("MOX-fuel", "__PlutoniumEnergy__/graphics/icons/MOX-fuel.png", 32)
+        deadlock.add_stack("plutonium-rounds-magazine", "__PlutoniumEnergy__/graphics/icons/plutonium-rounds-magazine.png", 32, "ammo")
+        deadlock.add_stack("plutonium-cannon-shell", "__PlutoniumEnergy__/graphics/icons/plutonium-cannon-shell.png", 32, "ammo")
+        deadlock.add_stack("explosive-plutonium-cannon-shell", "__PlutoniumEnergy__/graphics/icons/explosive-plutonium-cannon-shell.png", 32, "ammo")
+    end
 end
-]]
