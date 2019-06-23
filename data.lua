d2d8b866b912aaad07f3402f41cf88c3952d8b5e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-06-23 10:51:50 +0300

    Completed TODO

--- data.lua
+++ data.lua
@@ -4,7 +4,3 @@ require("prototypes.entity.entities")             -- Other prototype types have 
 require("prototypes.item.item")
 require("prototypes.recipe.recipe")
 require("prototypes.technology.technology")
-
---TODO
--- Fix technologies ingredients, especially time.
--- Fix hardened electric mining drill recipe enabled by default
